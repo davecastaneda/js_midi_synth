227e80c135a37202e0fba9873ec428d7b563a5db
davecastaneda <webdeveloperdave@gmail.com>
2023-03-25 19:39:30 -0500

    Console log midi access success or failure

--- app.js
+++ app.js
@@ -1,3 +1,13 @@
 if (navigator.requestMIDIAccess) {
     navigator.requestMIDIAccess().then(success, failure);
 }
+
+// 4:30 Test
+
+function success(midiAccess) {
+    console.log(midiAccess);
+}
+
+function failure() {
+    console.log('Could not connect to MIDI');
+}
