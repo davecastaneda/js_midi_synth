8e7866cebcc430f3568d1e17193ffebec0107179
davecastaneda <webdeveloperdave@gmail.com>
2023-04-06 23:33:21 -0500

    Commented out the console.logs

--- app.js
+++ app.js
@@ -5,15 +5,14 @@ if (navigator.requestMIDIAccess) {
 // 8:11 Test
 
 function success(midiAccess) {
-    console.log(midiAccess);
+    // console.log(midiAccess);
     // midiAccess.onstatechange = updateDevices;
     midiAccess.addEventListener('statechange', updateDevices);
 }
 
 function updateDevices(event) {
-    console.log(event);
+    // console.log(event);
     console.log('Name: $(event.port.name), Brand: ${event.port.manufacturer}, State: ${event.port.state}, Type: ${event.port.type}');
-
 }
 
 function failure() {
